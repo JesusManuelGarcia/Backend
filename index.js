1227fd1bc05819918a85d99dbe1bc9a534de8084
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-04-28 21:26:40 -0600

    modifica a html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hostname = '192.168.50.176';
 const port = 3000;
 const server = http.createServer((req, res)=>{
 res.statusCode=200;
-res.setHeader('Content-Type', 'text/html');
+res.setHeader('Content-Type', 'text/plain');
 res.end('Hola Mundo\n');
 });
 server.listen(port, hostname,function () {
