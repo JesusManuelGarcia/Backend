947d65ebd108e8468cfe233ce46a6f3e54059cd5
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-04-28 21:37:57 -0600

    mensajes de error

--- server.js
+++ server.js
@@ -21,3 +21,24 @@ var mineTypes = {
     '.jpg': 'image/jpg',
     '.gif': 'image/gif',
 };
+fs.readFile(filePath, function(error, content){
+    if (error){
+        if(error.code == 'ENDENT'){
+            fs.readFile('./404.html', function(error, content){
+                response.writeHead(200, {'Content-Type': contentType});
+                response.end(content, 'utf-8');
+            });
+        }
+        else {
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error:');
+            response.end();
+        }
+    }
+    else{
+        response.writeHead(200,{ 'Content-Type': contentType});
+        response.end(content, 'utf-8');
+    }
+});
+//}).listen(3000);
+//console.log('server running at http://127.0.0.1:3000/');
